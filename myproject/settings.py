50d49cf3c885a56d81d56c1f19550ac2f6e553a3
rak07 <rakesh.c9066@gmail.com>
2026-03-18 22:46:08 +0530

    fixed static files properly

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,3 +84,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # DEFAULT PRIMARY KEY
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
